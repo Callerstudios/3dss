74e07f8ddb0b486eed85c237d8ed20c5ff79c2e7
callerstudios <busariroqeeb16@gmail.com>
2025-01-12 12:18:46 +0100

    created top part of body

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-import Box from './components/Box/Box';
+import "./App.css";
+import Box from "./components/Box/Box";
 
 function App() {
   return (
     <div className="App">
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
-      <Box/>
+      <div className="human">
+        <div className="head">
+          <Box width="50px" height="50px" />
+        </div>
+        <div className="body">
+          <div className="left-hand">
+            <Box width="20px" height="100px" />
+          </div>
+          <div className="right-hand">
+            <Box width="20px" height="100px" />
+          </div>
+          <div className="chest">
+            <Box width="50px" height="100px" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import "./Box.css"
+import React from "react";
+import "./Box.css";
 
 const Box = (props) => {
   return (
-    <div className='box'>
-        <div className="front-face">aa</div>
-        <div className="left-face">aa</div>
-        <div className="right-face">aa</div>
-        <div className="top-face">aa</div>
-        <div className="bottom-face">aa</div>
-        <div className="back-face">aa</div>
+    <div
+      className="box"
+      style={{ "--width": props.width, "--height": props.height }}
+    >
+      <div className="front-face"></div>
+      <div className="left-face"></div>
+      <div className="right-face"></div>
+      <div className="top-face"></div>
+      <div className="bottom-face"></div>
+      <div className="back-face"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Box
+export default Box;
