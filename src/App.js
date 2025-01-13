8c7d8f2256743a00a0fd691d9a4f3d3ea0e1ca26
callerstudios <busariroqeeb16@gmail.com>
2025-01-13 16:07:44 +0100

    fixing cube bottom bug

--- src/App.js
+++ src/App.js
@@ -12,22 +12,22 @@ function App() {
           <Box width="20px" height="15px" stretch="20px" />
         </div>
         <div className="body">
-          <div className="left-hand">
+          {/* <div className="left-hand">
             <div className="arm">
-              <Box width="19px" height="50px" stretch="30px" />
+              <Box width="19px" height="50px" stretch="20px" />
             <div className="hand">
-              <Box width="20px" height="60px" stretch="30px" />
+              <Box width="20px" height="60px" stretch="20px" />
             </div>
             </div>
-          </div>
-          <div className="chest">
-            <Box width="50px" height="100px" stretch="30px" />
-          </div>
+          </div> */}
+          {/* <div className="chest">
+            <Box width="60px" height="100px" stretch="20px" />
+          </div> */}
           <div className="right-hand">
             <div className="arm">
-              <Box width="20px" height="50px" stretch="30px" />
+              <Box width="20px" height="50px" stretch="20px" />
             <div className="hand">
-              <Box width="20px" height="60px" stretch="30px" />
+              <Box width="20px" height="60px" stretch="20px" />
             </div>
             </div>
           </div>
