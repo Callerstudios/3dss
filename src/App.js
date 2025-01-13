a6f1d31f029f9eb1336d115bbcdd812db3056f2c
callerstudios <busariroqeeb16@gmail.com>
2025-01-13 18:25:23 +0100

    created human

--- src/App.js
+++ src/App.js
@@ -9,30 +9,37 @@ function App() {
           <Box width="50px" height="50px" stretch="30px" />
         </div>
         <div className="neck">
-          <Box width="20px" height="15px" stretch="20px" />
+          <Box width="20px" height="15px" stretch="10px" />
         </div>
         <div className="body">
-          {/* <div className="left-hand">
+          <div className="left-hand">
             <div className="arm">
               <Box width="19px" height="50px" stretch="20px" />
-            <div className="hand">
-              <Box width="20px" height="60px" stretch="20px" />
+              <div className="hand">
+                <Box width="20px" height="60px" stretch="20px" />
+              </div>
             </div>
-            </div>
-          </div> */}
-          {/* <div className="chest">
+          </div>
+          <div className="chest">
             <Box width="60px" height="100px" stretch="20px" />
-          </div> */}
+          </div>
           <div className="right-hand">
             <div className="arm">
               <Box width="20px" height="50px" stretch="20px" />
-            <div className="hand">
-              <Box width="20px" height="60px" stretch="20px" />
-            </div>
+              <div className="hand">
+                <Box width="20px" height="60px" stretch="20px" />
+              </div>
             </div>
           </div>
         </div>
-        <div className="legs"></div>
+        <div className="legs">
+          <Box width="20px" height="60px" stretch="20px" />
+          <Box width="20px" height="60px" stretch="20px" />
+        </div>
+        <div className="legs">
+          <Box width="20px" height="60px" stretch="20px" />
+          <Box width="20px" height="60px" stretch="20px" />
+        </div>
       </div>
     </div>
   );
