282a33480a37bc6431554be33dc27d21d37f6e24
callerstudios <busariroqeeb16@gmail.com>
2025-01-15 20:32:32 +0100

    Working on responsiveness

--- src/components/Swing/Swing.js
+++ src/components/Swing/Swing.js
@@ -1,42 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "../Box/Box";
 import "./Swing.css";
 import Human from "../Human/Human";
 
 const Swing = () => {
+  const [width, setWidth] = useState(window.innerWidth / 2);
+  const handleResize = () => {
+    setWidth(window.innerWidth / 2);
+  };
+  useEffect(() => console.log(width), []);
+  window.addEventListener("resize", handleResize);
   return (
     <div className="swing">
       <div className="horizontal">
-        <Box width="600px" height="10px" stretch="10px" />
+        <Box width={width + "px"} height="10px" stretch="10px" />
       </div>
       <div className="stands">
         <div className="stand1">
-          <Box width="500px" height="10px" stretch="10px" />
+          <Box width={width * 0.9 + "px"} height="10px" stretch="10px" />
         </div>
         <div className="stand2">
-          <Box width="500px" height="10px" stretch="10px" />
+          <Box width={width * 0.9 + "px"} height="10px" stretch="10px" />
         </div>
       </div>
-      <div className="stands-two">
+      <div
+        className="stands-two"
+        style={{
+          transform: `rotateZ(90deg) translateY(calc(${-width}px)) `,
+        }}
+      >
         <div className="stand1">
-          <Box width="500px" height="10px" stretch="10px" />
+          <Box width={width * 0.9 + "px"} height="10px" stretch="10px" />
         </div>
         <div className="stand2">
-          <Box width="500px" height="10px" stretch="10px" />
+          <Box width={width * 0.9 + "px"} height="10px" stretch="10px" />
         </div>
       </div>
       <div className="rope">
-        <div className="left">
-          <Box width="350px" height="10px" stretch="10px" />
+        <div className="left" style={{transform: `rotateZ(90deg) translateY(${(-width/2) -(width/6)}px)`}}>
+          <Box width={width * 0.5 + "px"} height="10px" stretch="10px" />
         </div>
-        <div className="right">
-          <Box width="350px" height="10px" stretch="10px" />
+        <div className="right" style={{transform: `rotateZ(90deg) translateY(${(-width/2) +(width/6) }px)`}}>
+          <Box width={width * 0.5 + "px"} height="10px" stretch="10px" />
           <div className="seat">
-            <Box width="200px" height="10px" stretch="100px" />
+            <Box width={width * 0.2 + "px"} height="10px" stretch="100px" />
           </div>
-          <div className="human2">
+          {/* <div className="human2">
             <Human />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
